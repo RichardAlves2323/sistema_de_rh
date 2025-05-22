9456bffc5af4a7d2e58eb79e8c2297b9bd2951a8
Richard Alves <richardalves232323@gmail.com>
2025-05-22 20:34:58 -0300

    :sparkles: feat: Add methods to retrieve all employees and find by ID through the controller

--- backend/src/main/java/com/rh/backend/api/controllers/employee/EmployeeController.java
+++ backend/src/main/java/com/rh/backend/api/controllers/employee/EmployeeController.java
@@ -3,13 +3,14 @@ package com.rh.backend.api.controllers.employee;
 import com.rh.backend.api.dtos.EmployeeDTO;
 import com.rh.backend.domain.entities.employee.Employee;
 import com.rh.backend.domain.services.employee.CreateEmployeeService;
+import com.rh.backend.domain.services.employee.FindEmployeeByIdService;
+import com.rh.backend.domain.services.employee.ListEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employee")
@@ -18,13 +19,32 @@ public class EmployeeController {
     @Autowired
     private CreateEmployeeService createEmployeeService;
 
-    @PostMapping("/create")
-    public ResponseEntity<Employee>  execute(@RequestBody EmployeeDTO employeeDTO) {
+    @Autowired
+    private ListEmployeeService listEmployeeService;
 
+    @Autowired
+    private FindEmployeeByIdService findEmployeeByIdService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Employee>  create(@RequestBody EmployeeDTO employeeDTO) {
         Employee newEmployee = new Employee(employeeDTO.fistName(), employeeDTO.lastName(), employeeDTO.email());
 
         Employee createdEmployee = this.createEmployeeService.execute(newEmployee);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(createdEmployee);
     }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Employee>> findAll() {
+        List<Employee> employees = this.listEmployeeService.execute();
+
+        return ResponseEntity.status(HttpStatus.OK).body(employees);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> findById(@PathVariable String id) {
+        Employee employee = this.findEmployeeByIdService.execute(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(employee);
+    }
+
 }
